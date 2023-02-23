3a0aaebedaed319237f1d30ad164d517e9302554
Georg Wölflein <georgw7777@gmail.com>
2023-02-23 13:31:25 +0000

    Train on mnist-bags

--- mil/utils/visualize.py
+++ mil/utils/visualize.py
@@ -4,6 +4,8 @@ import torch
 
 from ..data.mnist import Bag, unnormalize, make_collage
 
+SIGNIFICANT_ATTENTION_THRESHOLD = .1
+
 
 def label2char(label: bool) -> str:
     if label is None:
@@ -17,17 +19,17 @@ def red(s: str, show: bool = True) -> str:
     return f"\033[91m{s}\033[0m"
 
 
-def print_one_hot_bag(bag: Bag, y_pred=None):
+def print_one_hot_bag(bag: Bag, y_pred: torch.Tensor = None):
     bag_str = " ".join(red(instance_label, show=key_instance)
                        for instance_label, key_instance in zip(bag.instance_labels, bag.key_instances))
     bag_str = f"{{{bag_str}}}"
     print(f"{label2char(bag.bag_label)}{label2char(y_pred)} bag: {bag_str}")
 
 
-def print_one_hot_bag_with_attention(bag: Bag, attention: torch.Tensor, y_pred=None):
+def print_one_hot_bag_with_attention(bag: Bag, attention: torch.Tensor, y_pred: torch.Tensor = None):
     bag_str = " | ".join(red(f"{instance_label:4d}", show=key_instance)
                          for instance_label, key_instance in zip(bag.instance_labels, bag.key_instances))
-    att_str = " | ".join(red(f"{a:.2f}", show=a > .1)
+    att_str = " | ".join(red(f"{a:.2f}", show=a > SIGNIFICANT_ATTENTION_THRESHOLD)
                          for a in attention.squeeze(-1))
     bag_line = f"{label2char(bag.bag_label)} bag | {bag_str} |"
     att_line = f"{label2char(y_pred)} att | {att_str} |"
@@ -35,7 +37,7 @@ def print_one_hot_bag_with_attention(bag: Bag, attention: torch.Tensor, y_pred=N
     print(att_line)
 
 
-def plot_collage(bag: Bag, collage_size: int = None, highlight_key_instances: bool = True):
+def plot_collage(bag: Bag, highlight_key_instances: bool = True, collage_size: int = None, y_pred: torch.Tensor = None, attention: torch.Tensor = None):
     collage_img = make_collage(bag, collage_size=collage_size)
     collage_img = unnormalize(collage_img.unsqueeze(0)).squeeze(0).numpy()
     key_instance_mask = np.zeros_like(collage_img, dtype=bool)
@@ -49,31 +51,43 @@ def plot_collage(bag: Bag, collage_size: int = None, highlight_key_instances: bo
     collage_img = np.clip(collage_img, 0., 1.)
     plt.figure()
     plt.imshow(collage_img)
-    plt.gcf().suptitle(f"Bag label: {bag.bag_label.item() == 1.}")
+    plt.gcf().suptitle(f"Bag label: {bag.bag_label.item():.0f}")
     plt.axis("equal")
 
 
-def visualize_bag(bag: Bag, highlight_key_instances: bool = True, collage_size: int = None):
+def plot_bag(bag: Bag, highlight_key_instances: bool = True, collage_size: int = None, y_pred: torch.Tensor = None, attention: torch.Tensor = None):
     bag_label, instance_labels, key_instances, instances, instance_locations = bag
     if len(instances.shape) < 2 or instances.shape[-1] != instances.shape[-2]:
         raise ValueError(
             "Instances must be square images. Did you supply one-hot encoded bags?")
     if instance_locations is None:
         fig, axs = plt.subplots(1, instances.shape[0], figsize=(8, 2))
-        for instance, instance_label, key_instance, ax in zip(instances, instance_labels, key_instances, axs):
+        if attention is None:
+            attention = [None] * instances.shape[0]
+        for instance, instance_label, key_instance, att, ax in zip(instances, instance_labels, key_instances, attention, axs):
             instance = unnormalize(instance) * 255
             instance = instance.squeeze(0).numpy().astype(np.uint8)
             instance = np.repeat(instance[:, :, np.newaxis], 3, axis=2)
             if highlight_key_instances and key_instance:
                 instance[:, :, 0] = 255
             ax.imshow(instance)
-            ax.set_axis_off()
+            ax.xaxis.set_ticks([])
+            ax.yaxis.set_ticks([])
             ax.set_title(f"{instance_label}")
-        plt.gcf().suptitle(f"Bag label: {bag_label.item() == 1.}")
+            if att is not None:
+                ax.set_xlabel(f"{att:.2f}")
+                if att > SIGNIFICANT_ATTENTION_THRESHOLD:
+                    ax.xaxis.label.set_color("red")
+        title = f"Bag label: {bag_label.item():.0f}"
+        if y_pred is not None:
+            title += f", pred: {y_pred.item():.2f}"
+        plt.gcf().suptitle(title)
     else:
         plot_collage(bag,
                      highlight_key_instances=highlight_key_instances,
-                     collage_size=collage_size)
+                     collage_size=collage_size,
+                     y_pred=y_pred,
+                     attention=attention)
 
 
 def plot_attention_head(bag: Bag, A: torch.Tensor):
