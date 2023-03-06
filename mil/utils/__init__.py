4822a56ea0cc8d4820cc795ab3457473307ef1f5
Georg Wölflein <georgw7777@gmail.com>
2023-03-06 14:32:57 +0000

    Implement set transformer

--- mil/utils/__init__.py
+++ mil/utils/__init__.py
@@ -1,1 +1,11 @@
 from .device import DEVICE, device, detach
+
+
+def human_format(num: float) -> str:
+    """Display a large number in a human readable format. https://stackoverflow.com/a/45846841"""
+    num = float('{:.3g}'.format(num))
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
